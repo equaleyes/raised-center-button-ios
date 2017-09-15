8a4a442e937a84efd0b4ad5027dd1eec3ea7ed6f
Dejan Skledar <dejan.skledar@equaleyes.com>
2017-09-15 11:00:37 +0200

    feature: raised center button solution

---
+++ TabBarSolution/MainTabBar.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  TabBarSolution
+//
+//  Created by Dejan Skledar on 03/07/2017.
+//  Copyright © 2017 FERI. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBar {
+
+    private var middleButton = UIButton()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupMiddleButton()
+    }
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let from = point
+        let to = middleButton.center
+
+        return sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)) <= 39 ? middleButton : super.hitTest(point, with: event)
+    }
+
+    func setupMiddleButton() {
+        middleButton.frame.size = CGSize(width: 70, height: 70)
+        middleButton.backgroundColor = .blue
+        middleButton.layer.cornerRadius = 35
+        middleButton.layer.masksToBounds = true
+        middleButton.center = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+        middleButton.addTarget(self, action: #selector(test), for: .touchUpInside)
+        addSubview(middleButton)
+    }
+
+    func test() {
+        print("my name is jeff")
+    }
+}
---
+++ TabBarSolution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarSolution
+//
+//  Created by Dejan Skledar on 03/07/2017.
+//  Copyright © 2017 FERI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
